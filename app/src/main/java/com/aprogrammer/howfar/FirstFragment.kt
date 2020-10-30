537358005b75bc3d82884fd91c29982a03288b5f
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2020-10-30 14:39:01 +0530

    Added About me option and App icon

--- app/src/main/java/com/aprogrammer/howfar/FirstFragment.kt
+++ app/src/main/java/com/aprogrammer/howfar/FirstFragment.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -130,7 +129,7 @@ class FirstFragment : Fragment() {
                 myIntent.putParcelableArrayListExtra("locations", ArrayList<Parcelable>(places))
                 requireActivity().startActivity(myIntent)
             }) {
-                Log.d(tag, it.localizedMessage!!)
+
             }
     }
 
